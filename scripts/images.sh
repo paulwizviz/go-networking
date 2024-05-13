e6920d2205aeb04a4672517cb673efea997184a3
Paul Sitoh <paulwizviz@gmail.com>
2024-05-13 19:17:31 +0100

    Update playground example
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- scripts/images.sh
+++ scripts/images.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-export WEBSERVER_IMAGE=go-networking/webserver:current
-export HTTPUTIL_PROXY_IMAGE=go-networking/httputilproxy:current
-export CUSTOM_PROXY_IMAGE=go-networking/customproxy:current
-export P2P_NODE_IMAGE=go-networking/p2pnode:current
+# Playground
 export PLAYGROUND_IMAGE=go-networking/playground:current
-
 function build_playground(){
     docker-compose -f ./build/builder.yml build playground
 }
 
+function clean_playground(){
+    docker rmi -f ${PLAYGROUND_IMAGE}
+    docker rmi -f $(docker images --filter "dangling=true" -q)
+}
+
+# P2P
+export P2P_NODE_IMAGE=go-networking/p2pnode:current
 function build_p2pnode(){
     docker-compose -f ./build/builder.yml build p2pnode
-}
-
-function build_proxy(){
-    docker-compose -f ./build/builder.yml build webserver
-    docker-compose -f ./build/builder.yml build httputilproxy
-    docker-compose -f ./build/builder.yml build customproxy
-}
-
-function build_image(){
-    docker-compose -f ./build/builder.yml build
 }
 
 function clean_p2pnode(){
@@ -29,8 +22,14 @@ function clean_p2pnode(){
     docker rmi -f $(docker images --filter "dangling=true" -q)
 }
 
-function clean_playground(){
-    docker rmi -f ${PLAYGROUND_IMAGE}
+# Proxy
+export WEBSERVER_IMAGE=go-networking/webserver:current
+export HTTPUTIL_PROXY_IMAGE=go-networking/httputilproxy:current
+export CUSTOM_PROXY_IMAGE=go-networking/customproxy:current
+function build_proxy(){
+    docker-compose -f ./build/builder.yml build webserver
+    docker-compose -f ./build/builder.yml build httputilproxy
+    docker-compose -f ./build/builder.yml build customproxy
 }
 
 function clean_proxy(){
@@ -39,54 +38,3 @@ function clean_proxy(){
     docker rmi -f ${CUSTOM_PROXY_IMAGE}
     docker rmi -f $(docker images --filter "dangling=true" -q)
 }
-
-
-function clean_image(){
-    clean_playground
-    clean_p2p
-    clean_proxy
-    docker rmi -f $(docker images --filter "dangling=true" -q)
-}
-
-# function image(){
-#     local cmd="$1"
-#     case $cmd in
-#         "build:playground")
-#             build_playground
-#             ;;
-#         "build:proxy")
-#             build_proxy
-#             ;;
-#         "build:p2pnode")
-#             build_p2pnode
-#             ;;
-#         "build")
-#             build_image
-#             ;;
-#         "clean:playground")
-#             clean_playground
-#             ;;
-#         "clean:proxy")
-#             clean_proxy
-#             ;;
-#         "clean:p2pnode")
-#             clean_p2pnode
-#             ;;
-#         "clean")
-#             clean_image
-#             ;;
-#         *)
-#             echo "image [command]
-            
-# command:
-#     build:playground   playground image
-#     build:proxy        proxy image
-#     build:p2pnode      p2p node image
-#     build              all images
-#     clean:playground   playground image
-#     clean:proxy        proxy image
-#     clean:p2pnode      p2p node image
-#     clean              all images"
-#             ;;
-#     esac
-# }
---
+++ scripts/playground.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+if [ "$(basename $(realpath .))" != "go-networking" ]; then
+    echo "You are outside of the project"
+    exit 0
+else
+    . ./scripts/images.sh
+fi
+
+COMMAND=$1
+SUBCOMMAND=$2
+
+export NETWORK="go-networking_playground-network"
+export PLAYGROUND_VOLUME="go-network_playground-vol"
+export PLAYGROUND_CONTAINER="playground"
+
+function image(){
+    local cmd=$1
+    case $cmd in
+        "build")
+            build_playground
+            ;; 
+        "clean")
+            clean_playground 
+            ;;
+        *)
+            echo "Usage: $0 image [command]
+            
+command:
+    build   image
+    clean   remove images from cache"
+    esac
+}
+
+function ops(){
+    local cmd=$1
+    case $cmd in
+        "shell")
+            docker-compose -f ./deployments/playground.yaml run --rm -it playground /bin/sh
+            ;;
+        "clean")
+            docker container rm ${PLAYGROUND_CONTAINER}
+            docker volume rm ${PLAYGROUND_VOLUME}
+            ;;
+        *)
+            echo "Usage: $0 ops [command]
+            
+command:
+    shell   into playground container
+    clean   network artefacts"
+            ;;
+    esac
+}
+
+case $COMMAND in
+    "images")
+        image $SUBCOMMAND
+        ;;
+    "ops")
+        ops $SUBCOMMAND
+        ;;
+    *)
+        echo "Usage: $0 [command]
+        
+command:
+    images  related operations
+    ops     network operations"
+        ;;
+esac
