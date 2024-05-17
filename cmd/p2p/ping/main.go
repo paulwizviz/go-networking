636df568664f577a1ad6ffec0289a548d089a8e0
Paul Sitoh <paulwizviz@gmail.com>
2024-05-17 22:35:35 +0100

    Update cmd
    Fix P2P and ping application. Log exit net interface. Add comments.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- cmd/p2p/ping/main.go
+++ cmd/p2p/ping/main.go
@@ -23,13 +23,16 @@ func main() {
 	laddr := flag.String("laddr", "", "Listener address")
 	flag.Parse()
 
-	ad, err := net.InterfaceAddrs()
+	// Get IP address from network
+	netAddr, err := net.InterfaceAddrs()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	maddr := fmt.Sprintf("/ip4/%s/tcp/%d", strings.Split(ad[1].String(), "/")[0], *port)
+	// Prepare multiaddress format from network and port
+	maddr := fmt.Sprintf("/ip4/%s/tcp/%d", strings.Split(netAddr[1].String(), "/")[0], *port)
 
+	// instantiate a peer node
 	node, err := libp2p.New(
 		libp2p.ListenAddrStrings(maddr),
 		libp2p.Ping(false),
@@ -39,35 +42,47 @@ func main() {
 	}
 	defer node.Close()
 
+	// Instantiate pinging service
 	ps := &ping.PingService{Host: node}
 	node.SetStreamHandler(ping.ID, ps.PingHandler)
 
-	pi := peer.AddrInfo{
+	// Instantiate a peer info
+	peerInfo := peer.AddrInfo{
 		ID:    node.ID(),
 		Addrs: node.Addrs(),
 	}
-	addrs, err := peer.AddrInfoToP2pAddrs(&pi)
+
+	// Get Multi Addresses from peer info
+	addrs, err := peer.AddrInfoToP2pAddrs(&peerInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Listerner address: %v\n", addrs[0])
 
+	// Local address of listener peer
 	if *laddr != "" {
+
+		// Instantiate multi address from listener address
 		addr, err := multiaddr.NewMultiaddr(*laddr)
 		if err != nil {
 			log.Fatal(err)
 		}
-		peer, err := peer.AddrInfoFromP2pAddr(addr)
+
+		// Instantiate peer info
+		p, err := peer.AddrInfoFromP2pAddr(addr)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		err = node.Connect(context.Background(), *peer)
+		// Connect current node to listener
+		err = node.Connect(context.Background(), *p)
 		if err != nil {
 			log.Fatal("Connect", err)
 		}
+
+		// Send ping to listener
 		fmt.Println("Sending 5 ping to ", addr)
-		ch := ps.Ping(context.Background(), peer.ID)
+		ch := ps.Ping(context.Background(), p.ID)
 		for i := 0; i < 5; i++ {
 			res := <-ch
 			fmt.Println("pinged", addr, "in", res.RTT)
